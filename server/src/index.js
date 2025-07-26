dce6bcdecc0ef7a81977e9c363826d152fdf3b99
Tejassri Avinasha <tejassriavinasha@gmail.com>
2025-07-26 20:19:50 +0530

    basic app

--- server/src/index.js
+++ server/src/index.js
@@ -59,7 +59,9 @@ app.use((req, res, next) => {
     console.log(`${new Date().toISOString()} - ${req.method} ${req.url}`);
     next();
 });
-
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
 // API Routes
 app.use('/api', apiRoutes);
 
